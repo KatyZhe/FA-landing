50e0186f594b2273a700bfc524aee9a4fb7e0dc1
Katy_Zhe <nekhaeva.e@yandex.ru>
2023-07-11 19:12:30 +0300

    header beginning

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import "./Header.css";
+import logo from "../../logo.svg";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <section className="header__block">
+        <Link to="/" className="header__link">
+          <img src={logo} alt="логотип заголовка" className="header__logo" />
+        </Link>
+        <nav className="header__menu">
+          <Link to="/about" className="header__link">
+            <p>О нас</p>
+          </Link>
+          <Link to="/services" className="header__link">
+            <p>Услуги</p>
+          </Link>
+          <Link to="/articles" className="header__link">
+            <p>Статьи</p>
+          </Link>
+        </nav>
+        <button className="header__button">Напишите нам</button>
+      </section>
+      <section className="header__title">
+        <h1>Возьмите ваши финансы под контроль</h1>
+        <Link to="/contacts" className="header__contacts">
+          <p>Запланируйте финансовую консультацию</p>
+        </Link>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
