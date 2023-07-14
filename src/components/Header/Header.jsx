ee6572c17d79008253805460c85c9b75ba3b7a9c
Katy_Zhe <nekhaeva.e@yandex.ru>
2023-07-14 21:50:32 +0300

    header с бургером, блок Услуги

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,21 @@ import './Header.scss';
 import logo from '../../logo.png';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ openPopup, popup }) => {
   return (
     <header className="header">
       <section className="header__block">
         <Link to="/" className="header__link">
           <img src={logo} alt="логотип заголовка" className="header__logo" />
         </Link>
+        <div
+          className={`header__burger ${popup ? 'header__burger_pop' : ''}`}
+          onClick={openPopup}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <nav className="header__menu">
           <Link to="/about" className="header__link">
             <p>О нас</p>
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,54 +1,67 @@
-import './Service.scss';
+import "./Service.scss";
 
 const Service = () => {
   return (
     <section className="service">
       <h2 className="service__title">Наши услуги</h2>
+      <p className="service__about-service">Нажмите на услугу, чтобы увидеть подробное описание</p>
       <ul className="service__block">
         <li className="service__item">
-          <h3 class="service__item-title">Lorem ipsum</h3>
-          <p class="service__item-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <details>
+            <summary class="service__item-title"><h3 className="service__item-h3">Lorem ipsum</h3><div className="service__dotted"></div><p className="service__item-p">от 30 EUR</p></summary>
+            <p class="service__item-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </details>
         </li>
         <li className="service__item">
-          <h3 class="service__item-title">Lorem ipsum</h3>
-          <p class="service__item-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <details>
+            <summary class="service__item-title"><h3 className="service__item-h3">Lorem ipsum</h3><div className="service__dotted"></div><p className="service__item-p">от 30 EUR</p></summary>
+            <p class="service__item-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </details>
         </li>
         <li className="service__item">
-          <h3 class="service__item-title">Lorem ipsum</h3>
-          <p class="service__item-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <details>
+            <summary class="service__item-title"><h3 className="service__item-h3">Lorem ipsum</h3><div className="service__dotted"></div><p className="service__item-p">от 30 EUR</p></summary>
+            <p class="service__item-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </details>
         </li>
         <li className="service__item">
-          <h3 class="service__item-title">Lorem ipsum</h3>
-          <p class="service__item-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <details>
+            <summary class="service__item-title"><h3 className="service__item-h3">Lorem ipsum</h3><div className="service__dotted"></div><p className="service__item-p">от 30 EUR</p></summary>
+            <p class="service__item-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </details>
         </li>
         <li className="service__item">
-          <h3 class="service__item-title">Lorem ipsum</h3>
-          <p class="service__item-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <details>
+            <summary class="service__item-title"><h3 className="service__item-h3">Lorem ipsum</h3><div className="service__dotted"></div><p className="service__item-p">от 30 EUR</p></summary>
+            <p class="service__item-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </details>
         </li>
         <li className="service__item">
-          <h3 class="service__item-title">Lorem ipsum</h3>
-          <p class="service__item-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <details>
+            <summary class="service__item-title"><h3 className="service__item-h3">Lorem ipsum</h3><div className="service__dotted"></div><p className="service__item-p">от 30 EUR</p></summary>
+            <p class="service__item-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </details>
         </li>
       </ul>
-      <button className='service__button'>Узнать больше</button>
+      <button className="service__button">Узнать больше</button>
     </section>
   );
 };
