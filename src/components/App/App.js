48ac82d03dd80ca566cc7aff28f638e13c528999
Fra1zed <antonlaid@yandex.ru>
2023-07-17 11:41:28 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,21 @@ import Contact from '../Contact/Contact';
 import Main from '../Main/Main';
 import AboutUs from '../AboutUs/AboutUs';
 import { Route, Routes, useLocation } from 'react-router-dom/dist';
+import { useState } from 'react';
 
 function App() {
   let location = useLocation();
   const pathname = ['/contact'];
+  const [popup, setPopup] = useState(false);
+
+  const handlePopup = () => {
+    setPopup(!popup);
+  };
 
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header openPopup={handlePopup} popup={popup} />
+
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<AboutUs />} />
