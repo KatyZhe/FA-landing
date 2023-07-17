52759c25772b55488c87a53eb623c6fb3e21d693
Fra1zed <antonlaid@yandex.ru>
2023-07-17 12:14:16 +0300

    add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,25 @@
-import './Header.css';
+import './Header.scss';
 import logo from '../../logo.svg';
 import { Link } from 'react-router-dom';
+import rus from '../../images/russia-flag.svg';
+import eng from '../../images/british-flag-one.svg';
+import es from '../../images/british-flag-two.svg';
 
-const Header = () => {
+const Header = ({ openPopup, popup }) => {
   return (
     <header className="header">
       <section className="header__block">
         <Link to="/" className="header__link">
           <img src={logo} alt="логотип заголовка" className="header__logo" />
         </Link>
+        <div
+          className={`header__burger ${popup ? 'header__burger_pop' : ''}`}
+          onClick={openPopup}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <nav className="header__menu">
           <Link to="/about" className="header__link">
             <p>О нас</p>
@@ -21,6 +32,14 @@ const Header = () => {
           </Link>
         </nav>
         <button className="header__button">Напишите нам</button>
+        <div className="header__langs-button">
+          <div className="header__langs">
+            <img className="header__flag" src={rus} alt="флаг России" />
+            <img className="header__flag" src={eng} alt="флаг Великобритании" />
+            <img className="header__flag" src={es} alt="флаг Испании" />
+          </div>
+          <button className="header__button">Напишите нам</button>
+        </div>
       </section>
     </header>
   );
